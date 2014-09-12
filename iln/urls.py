63fca92ac3c706119a4c3ba2cd1b14986fc82717
Sara Palmer <sepalme@emory.edu>
2014-09-12 15:00:50 -0400

    udpate

--- iln/urls.py
+++ iln/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -26,9 +24,7 @@ urlpatterns = patterns('iln_app.views',
     url(r'^illus-subj/(?P<subj_id>[^/]+)/$', 'subject_display', name='subject_display'),
     )
    
-
-
-#if settings.DEBUG:
-  #urlpatterns += staticfiles_urlpatterns(
-       #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT } ),
-    #)
+if settings.DEBUG:
+  urlpatterns += staticfiles_urlpatterns(
+       url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT } ),
+    )
